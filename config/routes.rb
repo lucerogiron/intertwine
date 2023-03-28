59491db5be2f8c2bf736cc77011cf3c823592016
lucerogiron <gironlucero98@gmail.com>
2023-03-28 03:19:23 -0700

    added new.html.erb file for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only [:new, :create]
+  resources :users
   resource :calendar, only: [:show], controller: :calendar
 
   root to: "calendar#show"
