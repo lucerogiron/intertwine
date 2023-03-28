50e7d1167b90ce75b30c2c9f47004ce8f2f6c628
lucerogiron <gironlucero98@gmail.com>
2023-03-28 03:48:13 -0700

    added user show.html.erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  before_action :require_login
+  helper_method :current_user
+
+  def require_login
+    redirect_to new_session_path unless session.include? :user_id
   end
 
-  helper_method :current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def authenticate_user
     redirect_to "/login", status: :see_other unless current_user
